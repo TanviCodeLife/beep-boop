61a4ba721da4f54de5e5d5b7fd3a2e5097dce43d
Tanvi Garg <tanvi.garg23@gmail.com>
2018-10-26 10:41:06 -0700

    check remainder for numbers divisible by 3 and replace with string

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,20 @@ function generateRangeOfNumbers(inputInteger){
 for(var i = 0; i <= inputInteger; i++){
   numbersArray = numbersArray.concat(i);
 }
- console.log(numbersArray)
  return numbersArray;
+}
+
+function checkDivisibleByThree(rangeArray){
+  // var arrayRaw = generateRangeOfNumbers(inputNumbInt);
+  var arrayResult = [];
+  for(var i = 1; i < rangeArray.length; i++ ){
+    if (rangeArray[i] % 3 === 0){
+      rangeArray[i] = "I'm sorry, Dave. I'm afraid I can't do that."
+    }
+  }
+  console.log(rangeArray);
+  console.log(arrayResult);
+  return arrayResult;
 }
 
 
@@ -16,7 +28,8 @@ $(document).ready(function(){
     var inputNumbInt = parseInt($("#inputNumb").val());
     console.log(inputNumbInt);
     var inputRange = generateRangeOfNumbers(inputNumbInt);
-    console.log(inputRange);
+    // console.log(inputRange);
+    checkDivisibleByThree(inputRange);
    //check for whether number or not
   });
 });
